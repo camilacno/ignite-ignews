fd0aa588f51cce8547a59f0b30b4ffbd103fa5cc
Camila Nepomuceno <camila.cno@gmail.com>
2021-10-14 10:01:18 -0300

    Subscribe button component

---
+++ src/components/SubscribeButton/subscribeButtonIndex.tsx
@@ -0,0 +1,9 @@
+import styles from './subscribeButtonStyles.module.scss';
+
+export default function SubscribeButton() {
+  return (
+    <button type='button' className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import SubscribeButton from '../components/SubscribeButton/subscribeButtonIndex';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
             <br />
             <span>for $9.90/month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src='/images/avatar.svg' alt='Girl coding' />
       </main>
