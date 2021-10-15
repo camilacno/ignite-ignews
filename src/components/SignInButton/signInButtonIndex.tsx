a5b98fcab22431ff04da44f7ef0a75119e72dadc
Camila Nepomuceno <camila.cno@gmail.com>
2021-10-15 16:04:17 -0300

    Netx github auth

--- src/components/SignInButton/signInButtonIndex.tsx
+++ src/components/SignInButton/signInButtonIndex.tsx
@@ -1,15 +1,32 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/client';
+
 import styles from './signInButtonStyles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return (
-    <button type='button' className={styles.signInButton}>
-      <FaGithub color={isUserLoggedIn ? '#04d361' : '#eba417'} />
-      {isUserLoggedIn ? 'Sign in with Github' : 'Camila Nepomuceno'}
-      <FiX className={styles.closeIcon} style={!isUserLoggedIn && { display: 'none' }} />
+  console.log('session', session);
+
+  return session ? (
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color={'#04d361'} />
+      {session.user.name}
+      <FiX className={styles.closeIcon} color='#737388' />
+    </button>
+  ) : (
+    <button
+      type='button'
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color={'#eba417'} />
+      Sign in with Github
     </button>
   );
 }
