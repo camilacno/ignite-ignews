64c3f2edfe37d5736a4afc64fc3895911f368c12
Camila Nepomuceno <camila.cno@gmail.com>
2021-10-26 20:08:34 -0300

    Listing posts from prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,23 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -14,23 +26,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href='#'>
-            <time>21 de outubro de 2021</time>
-            <strong>Post title</strong>
-            <p>Brief post's summary</p>
-          </a>
-
-          <a href='#'>
-            <time>21 de outubro de 2021</time>
-            <strong>Post title</strong>
-            <p>Brief post's summary</p>
-          </a>
-
-          <a href='#'>
-            <time>21 de outubro de 2021</time>
-            <strong>Post title</strong>
-            <p>Brief post's summary</p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -46,9 +48,21 @@ export const getStaticProps: GetStaticProps = async () => {
       pageSize: 50,
     }
   );
-  console.log('response', response);
-
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  });
   return {
-    props: {},
+    props: { posts },
   };
 };
