1d20822cc34cc7b0bc903c45b69026324c8489dc
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-21 21:38:33 -0300

    Change img to image (next/image) in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,18 @@
-import { SignInButton } from '../SignInButton';
-import { ActiveLink } from '../ActiveLink';
-import styles from './headerStyles.module.scss';
+import Image from 'next/image'
+import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
+import styles from './headerStyles.module.scss'
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src='/images/logo.svg' alt='ig.news' />
+        <Image src="/images/logo.svg" alt="ig.news" width={108} height={30} />
         <nav>
-          <ActiveLink activeClassName={styles.active} href='/'>
+          <ActiveLink activeClassName={styles.active} href="/">
             <a>Home</a>
           </ActiveLink>
-          <ActiveLink activeClassName={styles.active} href='/posts'>
+          <ActiveLink activeClassName={styles.active} href="/posts">
             <a>Posts</a>
           </ActiveLink>
         </nav>
@@ -19,5 +20,5 @@ export function Header() {
         <SignInButton />
       </div>
     </header>
-  );
+  )
 }
