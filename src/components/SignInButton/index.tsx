5c4ded075683f0e6ec766b6a568a0a65bc46a7df
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-25 10:50:58 -0300

    Fix linting

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,30 +1,30 @@
-import { FaGithub } from 'react-icons/fa';
-import { FiX } from 'react-icons/fi';
-import { signIn, useSession, signOut } from 'next-auth/client';
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/client'
 
-import styles from './signInButtonStyles.module.scss';
+import styles from './signInButtonStyles.module.scss'
 
 export function SignInButton() {
-  const [session] = useSession();
+  const [session] = useSession()
 
   return session ? (
     <button
-      type='button'
+      type="button"
       className={styles.signInButton}
       onClick={() => signOut()}
     >
-      <FaGithub color={'#04d361'} />
+      <FaGithub color={'#38A980'} />
       {session.user.name}
-      <FiX className={styles.closeIcon} color='#737388' />
+      <FiX className={styles.closeIcon} color="#737388" />
     </button>
   ) : (
     <button
-      type='button'
+      type="button"
       className={styles.signInButton}
       onClick={() => signIn('github')}
     >
       <FaGithub color={'#eba417'} />
       Sign in with Github
     </button>
-  );
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import Head from 'next/head'
 
-import { SubscribeButton } from '../components/SubscribeButton';
-import { stripe } from '../services/stripe';
-import styles from './home.module.scss';
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
+import styles from './home.module.scss'
 
 interface HomeProps {
   product: {
-    priceId: string;
-    amount: string;
-  };
+    priceId: string
+    amount: string
+  }
 }
 
 export default function Home({ product }: HomeProps) {
@@ -28,34 +29,40 @@ export default function Home({ product }: HomeProps) {
           <p>
             Get access to all publications
             <br />
-            <span>for {product.amount}/month</span>
+            {/* <span>for {product.amount}/month</span> */}
+            <span>for 49,90/month</span>
           </p>
           <SubscribeButton />
         </section>
-        <img src='/images/avatar.svg' alt='Girl coding' />
+        <Image
+          src="/images/avatar.svg"
+          alt="Girl coding"
+          height={520}
+          width={334}
+        />
       </main>
     </>
-  );
+  )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1JkXIOKNHdhAUyRAEkbJ1IMv', {
-    // expand: ['product'],
-    // used in cases where details of the product and / or several product pricing options are necessary
-  });
+// export const getStaticProps: GetStaticProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1JkXIOKNHdhAUyRAEkbJ1IMv', {
+//     // expand: ['product'],
+//     // used in cases where details of the product and / or several product pricing options are necessary
+//   })
 
-  const product = {
-    priceId: price.id,
-    amount: new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-    }).format(price.unit_amount / 100),
-  };
+// const product = {
+//   priceId: price.id,
+//   amount: new Intl.NumberFormat('en-US', {
+//     style: 'currency',
+//     currency: 'USD',
+//   }).format(price.unit_amount / 100),
+// }
 
-  return {
-    props: {
-      product,
-    },
-    revalidate: 60 * 60 * 24, //24 horas
-  };
-};
+// return {
+//   props: {
+//     product,
+//   },
+//   revalidate: 60 * 60 * 24, //24 horas
+// }
+// }
